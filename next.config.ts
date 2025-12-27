32b861e77ff4ba2ebc8da528f0d5a4b09f454fab
Jx123408 <jxjin.work@outlook.com>
2025-12-27 19:48:14 +0900

    fix the build error

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
-import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 };
 
-// @ts-ignore: next-pwa types might be missing
-export default withPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-})(nextConfig);
+export default nextConfig;
